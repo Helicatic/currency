f786928e927d6e831f89f5a58966e40c3af0f8f9
Helicatic <helicatic@users.noreply.github.com>
2020-10-05 23:06:19 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const inputUzs = document.querySelector("#uzs"),
+  inputUsd = document.querySelector("#usd");
+
+inputUzs.addEventListener("input", () => {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", "js/current.json");
+  request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  request.send();
+
+  request.addEventListener("load", () => {
+    if (request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputUzs.value / data.current.usd).toFixed(2);
+    } else {
+      inputUsd.value = "Что-то пошло не так, тахорат килиб кел";
+    }
+  });
+
+  // status
+  // StatusText
+  // response
+  // readyState
+});
+
+inputUsd.addEventListener("input", () => {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", "js/current.json");
+  request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  request.send();
+
+  request.addEventListener("load", () => {
+    if (request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUzs.value = (+inputUsd.value * data.current.usd).toFixed(2);
+    } else {
+      inputUzs.value = "Что-то пошло не так, тахорат килиб кел";
+    }
+  });
+
+  // status
+  // StatusText
+  // response
+  // readyState
+});
